5819f337f258a2941c62aff7013ce7004f0a7dfc
Paul Nilsson <paul.nilsson@cern.ch>
2021-09-09 10:10:37 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dask-scheduler
-Docker image scripts for a Dask scheduler
+Docker image scripts for a Dask scheduler.
+
+This image can be used to start a Dask scheduler. It is based on the continuumio/miniconda3:4.8.2 image and installs Dask version 2021.7.2, Python 3.8,
+numpy 1.21.1, pandas 1.3.0 as well as a few other tools.
---
+++ prepare.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Authors:
+# - Paul Nilsson, paul.nilsson@cern.ch, 2021
+
+set -x
+
+# We start by adding extra apt packages, since pip modules may required library
+#if [ "$EXTRA_APT_PACKAGES" ]; then
+#    echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
+#    apt update -y
+#    apt install -y $EXTRA_APT_PACKAGES
+#fi
+
+if [ -e "/opt/app/environment.yml" ]; then
+    echo "environment.yml found. Installing packages"
+    /opt/conda/bin/conda env update -f /opt/app/environment.yml
+    conda init bash
+    activate myenv
+else
+    echo "no environment.yml"
+fi
+
+#if [ "$EXTRA_CONDA_PACKAGES" ]; then
+#    echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
+#    /opt/conda/bin/conda install -y $EXTRA_CONDA_PACKAGES
+#fi
+
+#if [ "$EXTRA_PIP_PACKAGES" ]; then
+#    echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
+#    /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
+#fi
+
+# Run extra commands
+#exec "$@"
+
+echo "starting dask scheduler"
+dask-scheduler --idle-timeout 500
+echo "finished dask scheduler"
